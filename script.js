58a2301f38540875d0104e991a9ad0e69d97a9a3
saisantosh2805 <saisantosh2000naviri@gmail.com>
2025-03-19 14:34:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function getFormvalue() {
-    //Write your code here
-
+function getFormvalue(event) {
+  event.preventDefault();//Write your code here
+const form= document.getElementById("form1");  
+  const firstName=document.getElementById("fname").value;
+  const lastName=document.getElementById("lname").value;
+  alert(`${firstName}${lastName}`);
 }
