f9d580674c7f751e782ad3892b62188e86169808
saisantosh2805 <saisantosh2000naviri@gmail.com>
2025-03-19 15:36:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function getFormvalue(event) {
   event.preventDefault();//Write your code here
 const form= document.getElementById("form1");  
-  const firstName=document.getElementById("fname").value;
-  const lastName=document.getElementById("lname").value;
-  alert(`${firstName}${lastName}`);
+  const firstName=form.elements["fname"].value.trim();
+  const lastName=form.elements["lname"].value.trim();	 
+  if (!firstName || !lastName) {
+    alert("Please fill in both First Name and Last Name fields.");
+    return;
+  }
+  const fullName = ${firstName} ${lastName};	alert(`${firstName}${lastName}`);
+alert(fullName)
 }
