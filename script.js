89e5ce822806223fb2563569e806471996b462fb
saisantosh2805 <saisantosh2000naviri@gmail.com>
2025-03-19 15:54:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ function getFormvalue(event) {
 
  
   const firstName = form.elements["fname"].value.trim();
-  const middleName = form.elements["mname"].value.trim();
   const lastName = form.elements["lname"].value.trim();
 
   
   if (!firstName || !lastName) {
     alert("Please fill in both First Name and Last Name fields.");
     return;
-  } 
-	const fullName = middleName ? `${firstName} ${middleName} ${lastName}` : `${firstName} ${lastName}`;
+  }
+
+  const fullName = ${firstName} ${lastName};
+
+
   alert(fullName);
-}document.getElementById("form1").addEventListener("submit", getFormvalue);
+}
